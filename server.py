100ad057ce843e69265c5a33c9a14cc7810ee9eb
Lee-Kubrick <83480251+Lee-Kubrick@users.noreply.github.com>
2025-04-17 21:30:42 -0400

    Initial commit

---
+++ server.py
@@ -0,0 +1,28 @@
+import eventlet
+eventlet.monkey_patch()  # IMPORTANT: Do this before anything else
+
+from flask import Flask
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+print("Server is starting... Visit http://localhost:5000/")
+
+
+votes = {}
+
+@app.route('/')
+def index():
+    return "Server is running."
+
+@socketio.on('submit_vote')
+def handle_vote(data):
+    user_id = data['user_id']
+    choice = data['choice']
+    votes[user_id] = choice
+    emit('update_votes', votes, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, port=5000)
